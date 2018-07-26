0d2162a31e0e6a2a4fc054bc3b9ac27be28b85de
piochyz <chyzy29@gmail.com>
2018-07-26 15:58:13 +0200

    1. Przećwiczenie pętli for

---
+++ 0012_divisors_for_loop1.py
@@ -0,0 +1,18 @@
+''''
+1.Przećwiczenie petli for:
+
+Napisz program wypisujący wszystkie dzielniki zadanej liczby naturalnej, liczbę tych dzielników oraz ich sumę. Program
+powinien podawać komunikat, jeżeli podana liczba jest liczbą pierwszą.
+'''
+
+print("\nPodaj liczbę naturalną, której dzielniki chciałbyś poznać: ")
+
+liczba = int(input())
+
+dzielniki = [i for i in range(1, liczba+1) if liczba % i == 0]
+suma = 0
+for i in dzielniki :
+    suma += i
+
+print("Dzielników liczby {0} jest {1}, to: ".format(liczba, len(dzielniki)) +
+      str(dzielniki) + '\nA ich suma, to: {0}'.format(suma))
