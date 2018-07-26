66f2738be55c3f3440b4a682603ed0d075c2443a
piochyz <chyzy29@gmail.com>
2018-07-26 22:52:42 +0200

    Pętla for i tabliczka mnozenia Dodanie wstępnych informacji o NumPy packege

---
+++ 0014_multiplication_table_for_loop_3_&_numpy.py
@@ -0,0 +1,31 @@
+#Napisz program, który wyswietli tabliczkę mnożenia
+
+print('\n\t\tMultiplication Table\n')
+
+for i in range(1,11) :
+    for k in range(1, 11):
+        print(i * k, end='\t')
+    print('\n')
+#Przy większej ilości danych te petle nie bd tak wydajne jak NymPy
+
+#A teraz tablice. Napisz program, który ponumeruje pola w tablicy 4x4, od lewej do prawej \n x 4 :
+
+import numpy as np
+
+tab4x4 = np.linspace(1,16,16) # od 1 do 16 stwórz array 16 punktów
+
+tab4x4 = tab4x4.reshape([4,4]) # zmień kształt na 4x4
+tab4x4 = tab4x4.astype(int) #zmien typ na int
+
+print(tab4x4)
+print(tab4x4[1][3]) # sprawdzenie pozycjonowania
+
+# Numpy pozwala używać pythona wydajniej. Generowanie pętli zajmuje za dużo czasu - czesto x 100.
+
+nr =0
+for i in range(0,4) if i < 4 :
+    for k in range(1,17)  if k <4 :
+        nr += 1
+
+# Dodaj ćwiczenie 17b 17c i 17d - potrzebujesz numpy
+
