2d86dce913733d25142eca8ce90917092100a567
piochyz <chyzy29@gmail.com>
2018-07-30 11:27:00 +0200

    Tworzenie dict z dwóch list. Szybsze pisanie kodu.

--- 0008_switch_understanding.py
+++ 0008_switch_understanding.py
@@ -5,18 +5,16 @@ def main():
     print('Podaj nr dnia tygodnia...')
     nr_dnia = int(input())
 
+    #dane pod dict, key = dni, value = num:
+    dni = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela']
+    num = [num for num in range(1,8)]
+
     def switch_dzien_tyg(nr_dnia):
-        switcher = {
-            1 : "Poniedziałek",
-            2 : "Wtorek",
-            3 : "Środa",
-            4 : "Czwartek",
-            5 : "Piątek",
-            6 : "Sobota",
-            7 : "Niedziela"
-        }
+        switcher = dict(zip(num, dni)) #tworzy dict
         print(switcher.get(nr_dnia, "Niepoprawny dzień"))
+
     switch_dzien_tyg(nr_dnia)
+
     return 0
 
 main()
