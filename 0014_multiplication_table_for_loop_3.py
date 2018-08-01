ecdbbadff2119867579ed83220ab8d7d2bb8fb27
Piotr <chyzy29@gmail.com>
2018-08-01 14:44:54 +0200

    Delete 0014_multiplication_table_for_loop_3.py

--- 0014_multiplication_table_for_loop_3.py
+++
@@ -1,8 +0,0 @@
-#Napisz program, który wyswietli tabliczkę mnożenia
-
-print('\n\t\tMultiplication Table\n')
-
-for i in range(1,11) :
-    for k in range(1, 11):
-        print(i * k, end='\t')
-    print('\n')
