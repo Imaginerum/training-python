4c69f8ea1b082db29f76300714f6d475b32d4305
Piotr <chyzy29@gmail.com>
2022-03-31 14:30:35 +0200

    nowe zadanie i jego wielokrotne rozwiązania.

--- 0019_find_unique_number.py
+++ 0019_find_unique_number.py
@@ -14,6 +14,13 @@ def uniq3(q):
     a = sorted(q)
     return a[0] if a[0] != a[1] else a[-1]
 
+from collections import Counter
+def uniq4(q):
+    return Counter(q).most_common()[-1][0]
+
+def uniq(q):
+    q.sort()
+    return q[0] if q[0] != q[1] else q[-1]
 
 arr = [0,0,0,6,0,0,0,0,0]
 print(uniq(arr))
