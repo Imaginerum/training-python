52af53c29c2d513889a0f9f3c602df9f78058f85
Piotr <chyzy29@gmail.com>
2022-03-31 12:50:58 +0200

    zadanie dotyczące split string z wieloma rozwiązaniami problemu

--- 0017_split_strings.py
+++ 0017_split_strings.py
@@ -19,5 +19,35 @@ def solution3(s):
 
 def solution4(s):
     return [(s + "_")[i:i+2] for i in range(0, len(s), 2)]
+
+def solution5(s):
+    if len(s) % 2 == 1:
+        s += '_'
+    return [s[i:i+2] for i in range(0,len(s),2)]
+
+import re
+
+def solution6(s):
+    return re.findall('..', s + '_')
+
+def solution7(s):
+    if len(s) == 0:
+        return[]
+    elif len(s) == 1:
+        return [s+'_']
+    else:
+        return [s[:2]] + solution7(s[2:])
+
+def solution8(s):
+    s = s + '_' if (len(s)%2 == 1) else s
+    return [s[i:i+2] for i in range(0, len(s), 2)]
+
+def solution9(s):
+    if len(s) == 1:
+        return [s+"_"]
+    elif len(s) == 0:
+        return []
+    return [s[0] + s[1]] + solution9(s[2:])
+
 x= 'askJSKdnakd'
-print(solution4(x))
+print(solution9(x))
