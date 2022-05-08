d238fb334f11fe99eade2ea7b09590af0ec618e9
Piotr <chyzy29@gmail.com>
2022-05-08 13:22:53 +0200

    changing positioning

--- 0040_tic_tac_toe.py
+++ 0040_tic_tac_toe.py
@@ -1,16 +1,27 @@
-#tic tac toe the simplest it can be.
+#tic tac toe the simplest it can be: Player vs. player version.
 
-theBoard = {'top-L': " ", 'top-M': ' ', 'top-R': ' ',
-            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+theBoard = {'7': " ", '8': ' ', '9': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '1': ' ', '2': ' ', '3': ' '}
+
+def gamesControl():
+    print("\nSterowanie grą to: ", end='\n\n')
+    print("7|8|9")
+    print("-+-+-")
+    print("4|5|6")
+    print("-+-+-")
+    print("1|2|3", end='\n\n')
 
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print("Przebieg gry: \n")
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('–+–+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('–+–+–')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
-    print('\n')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print()
+
+gamesControl()
 
 turn = 'X'
 for i in range(9):
