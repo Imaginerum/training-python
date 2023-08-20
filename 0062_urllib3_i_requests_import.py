bc8eb02c621fd39836a30a503e7b803f6d24cc69
imaginerum <chyzy29@gmail.com>
2023-08-20 10:38:24 +0200

    używanie poczatkowe bibliotek requests i urllib3

--- 0062_urllib3_i_requests_import.py
+++ 0062_urllib3_i_requests_import.py
@@ -1,12 +1,20 @@
 import urllib3
+import requests
 
 http = urllib3.PoolManager()
 r = http.request("GET", 'https://github.com/imaginerum/python-training')
 print(f"Oto status strony {r.status}")
 print(f"Zawartość formatu\n{r.data.decode('utf-8')}")
 
-import requests
 
 req = requests.get("https://github.com/imaginerum/python-training")
 print(f"Strona jest {req.status_code}")
 print(f"zawartość {req.text}")
+
+
+payload = {"email":"dummy@mail.provider", "password":"password"}
+r2 = requests.post("https://me.hack.me/login", data=payload)
+if "logout" in r2.text:
+    print("logged in!")
+else:
+    print("Acces Denied")
